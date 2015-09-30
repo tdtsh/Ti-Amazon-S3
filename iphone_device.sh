593a8774949cf86f45ddd8f891f1e25df8d39f27
tdtsh <systemsfailing@gmail.com>
2015-10-01 03:10:53 +0900

    SDK 5.0.0.GA

---
+++ iphone_device.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+appc ti build -T device -p iphone --developer-name "tadatoshi hanazaki (7C5S9PMBAF)" --pp-uuid "82f6ed1c-a318-4f76-8ca5-e1050f5a2a5e" --device-id "bd8a22872f9116c4180f75f8c26d8d5fe4d3fb8a" --dev.tiapp dev
